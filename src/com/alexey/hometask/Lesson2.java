a4e5cb4faab48f4c459a374e45f2f6ddb35431ac
allekso <ajjekso@gmail.com>
2018-10-07 14:58:31 +0300

    Задача 1

--- src/com/alexey/hometask/Lesson2.java
+++ src/com/alexey/hometask/Lesson2.java
@@ -1,7 +1,25 @@
 package com.alexey.hometask;
+//Написать программу ввода с клавиатуры последовательно 3 чисел. Вывести минимальное максимальное и средние значения
+import java.util.OptionalDouble;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        System.out.println("s111");
-    }
-}
+        System.out.println("Введите 3 числа:");
+        Scanner scan = new Scanner(System.in);
+        int[] tab = new int[3];
+        for (int i = 0; i < 3; i++)
+            tab[i] = scan.nextInt();
+        int min = Arrays.stream(tab).min().getAsInt();
+        int max = Arrays.stream(tab).max().getAsInt();
+        OptionalDouble avg = Arrays.stream(tab).average();
+        System.out.println("Min = " + min);
+        System.out.println("Max = " + max);
+        System.out.println("Avg = " + avg);
+
+
+
+
+}}
+
