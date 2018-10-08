69baff21722d009e6c895019b90b2e781b06f69d
Allekso <ajjekso@gmail.com>
2018-10-08 22:44:58 +0300

    11

---
+++ src/com/alexey/hometask/Methods.java
@@ -0,0 +1,35 @@
+package com.alexey.hometask;
+
+import java.util.Scanner;
+
+public class Methods {
+
+    public static void main(String[] args) {
+        int max = inputNumber();
+        System.out.println(sum(max));
+        newMethod();
+    }
+
+    private static void newMethod() {
+    }
+
+    public static int inputNumber() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        while (n < 0) {
+            System.out.println("Введите число больше нуля");
+            n = scan.nextInt();
+
+        }
+        return n;
+    }
+    
+    public static int sum(int max) {
+        int sum = 0;
+        for (int i = 0; i <= max; i++)
+            sum = sum + i;
+        return  sum;
+    }
+
+}
+
