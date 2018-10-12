4c07cf0ae2fa85a79c617267f45783286f7c7d1c
Allekso <ajjekso@gmail.com>
2018-10-12 20:41:37 +0300

    merge step 3 fix second fix problem

--- src/com/telesens/academy/Lesson5/gitMerge.java
+++ src/com/telesens/academy/Lesson5/gitMerge.java
@@ -3,5 +3,6 @@ package com.telesens.academy.Lesson5;
 public class gitMerge {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("fix here too");
     }
 }
