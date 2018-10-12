728949b33ea935a3e678891ac15128d6ac6651ca
Allekso <ajjekso@gmail.com>
2018-10-12 20:42:17 +0300

    merge step 4 final feature

--- src/com/telesens/academy/Lesson5/gitMerge.java
+++ src/com/telesens/academy/Lesson5/gitMerge.java
@@ -4,5 +4,6 @@ public class gitMerge {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("new feature in master");
+        System.out.println("finat feature");
     }
 }
