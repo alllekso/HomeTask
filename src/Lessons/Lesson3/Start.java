fbc41e0447dc6abcc611e457361f08f4a888d359
allekso <ajjekso@gmail.com>
2018-10-17 12:49:40 +0300

    добавил чётные

--- src/Lessons/Lesson3/Start.java
+++ src/Lessons/Lesson3/Start.java
@@ -1,3 +1,33 @@
+//Создать новый проект/класс.
+//
+//        Создать метод createArrayDialog, который в диалоговом режиме
+//        запрашивает размер массива, все его элементы и возвращает
+//        созданный числовой массив
+//
+//        Создать метод sum, который вычисляет сумму элементов массива
+//
+//        Создать метод max, который определяет максимальный элемент
+//
+//        Создать метод countEven, который вычисляет сумму четных элементов
+//        массива
+//
+//        Создать метод printArray, который выводит массив на консоль
+//
+//        Создать метод printReport, который принимает в качестве аргументов
+//        размер массива, сумму, макс, кол-во четных элементов и выводит
+//        отчет на консоль:
+//
+//        Размер: 3
+//
+//        Сумма: 18
+//
+//        Максимальный: 10
+//
+//        Кол-во четных: 2
+//
+//        Продемонстрировать работу программы (в методe main не должно быть
+//        другого кода, кроме вызовов созданных методов)
+
 package Lessons.Lesson3;
 
 import java.util.Arrays;
@@ -8,8 +38,14 @@ public class Start {
     static int[] array = createArrayDialog();
 
     public static void main(String[] args) {
+        printArray(array);
         System.out.println(sum(array));
+        System.out.println(max(array));
+        System.out.println(countEven(array));
+    }
 
+    private static void printArray(int[] array) {
+        System.out.println(Arrays.toString(array));
     }
 
     private static int[] createArrayDialog() {
@@ -22,7 +58,6 @@ public class Start {
             array[i] = scan.nextInt();
         }
         System.out.printf("Вы ввели массив из %d элементов: ", a);
-        System.out.println(Arrays.toString(array));
         return array;
     }
 
@@ -35,4 +70,21 @@ public class Start {
         return summa;
     }
 
+    public static int max(int[] array) {
+        Arrays.sort(array);
+        int maxID = array[array.length - 1];
+        System.out.print("Максимальное число массива: ");
+        return maxID;
+    }
+
+    private static int countEven(int[] array) {
+        int summaChetnix = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                summaChetnix = summaChetnix + array[i];
+            }
+        }
+        System.out.print("Сумма чётных чисел массива: ");
+        return summaChetnix;
+    }
 }
